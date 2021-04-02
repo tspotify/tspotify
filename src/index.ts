3ec146fc5bf0eb1790f9348953a8f236412f42c2
Shubham Parihar <shubhamparihar391@gmail.com>
2021-04-02 21:03:47 +0530

    refactor: expose exported classes from index for better documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
+import BaseClient from './client/BaseClient.js';
 import Client from './client/Client.js';
+import AlbumManager from './managers/AlbumManager.js';
+import BaseManager from './managers/BaseManager.js';
+import AccessTokenDetails from './structures/AccessTokenDetails.js';
+import Album from './structures/Album.js';
+import APIOptions from './structures/APIOptions.js';
+import Artist from './structures/Artist.js';
+import BaseAlbum from './structures/BaseAlbum.js';
+import BaseStructure from './structures/BaseStructure.js';
+import SimplifiedAlbum from './structures/SimplifiedAlbum.js';
+import SimplifiedArtist from './structures/SimplifiedArtist.js';
+import Collection from './util/Collection.js';
+export * from './util/Constants.js';
+export * from './util/Interfaces.js';
+export * from './util/Util.js';
 
-export { Client };
+export {
+  BaseClient,
+  Client,
+  AlbumManager,
+  BaseManager,
+  AccessTokenDetails,
+  Album,
+  APIOptions,
+  Artist,
+  BaseAlbum,
+  BaseStructure,
+  SimplifiedAlbum,
+  SimplifiedArtist,
+  Collection,
+};
