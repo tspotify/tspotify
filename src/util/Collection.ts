81bfc61e3377dd447f5a454d95280c2f6e7c6faf
Shubham Parihar <shubhamparihar391@gmail.com>
2021-04-02 20:54:48 +0530

    feat(Collection): add data structure for storing cache in managers

---
+++ src/util/Collection.ts
@@ -0,0 +1,8 @@
+import BaseCollection from '@discordjs/collection';
+
+/**
+ * Structure for storing cache in the managers
+ */
+export default class Collection<K, V> extends BaseCollection<K, V> {
+  // will add something here later
+}
