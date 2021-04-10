acdc77e7309e0bdedb69fc7a4ba35a92731419ee
Shubham Parihar <shubhamparihar391@gmail.com>
2021-04-10 23:56:01 +0530

    feat: export classes that were left unexported

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
 import BaseClient from './client/BaseClient.js';
 import Client from './client/Client.js';
+
 import AlbumManager from './managers/AlbumManager.js';
 import BaseManager from './managers/BaseManager.js';
+
 import AccessTokenDetails from './structures/AccessTokenDetails.js';
 import Album from './structures/Album.js';
 import APIOptions from './structures/APIOptions.js';
 import Artist from './structures/Artist.js';
 import BaseAlbum from './structures/BaseAlbum.js';
 import BaseStructure from './structures/BaseStructure.js';
+import LinkedTrack from './structures/LinkedTrack.js';
+export * from './structures/Misc.js';
 import SimplifiedAlbum from './structures/SimplifiedAlbum.js';
 import SimplifiedArtist from './structures/SimplifiedArtist.js';
+import SimplifiedTrack from './structures/SimplifiedTrack.js';
+import Track from './structures/Track.js';
+
 import Collection from './util/Collection.js';
 export * from './util/Constants.js';
 export * from './util/Interfaces.js';
 export * from './util/Util.js';
-export * from './structures/Misc.js';
 
 export {
   BaseClient,
@@ -27,7 +33,10 @@ export {
   Artist,
   BaseAlbum,
   BaseStructure,
+  LinkedTrack,
   SimplifiedAlbum,
   SimplifiedArtist,
+  SimplifiedTrack,
+  Track,
   Collection,
 };
